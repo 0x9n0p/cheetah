0d78945c7be706a904964376967f34b183a8bcba
Mohammad Amin Ahmadi <0x9n0p@gmail.com>
2023-10-21 17:11:17 +0330

    Migrate to BOOTBOOT so that no protected mode trampoline codes needed

--- Makefile
+++ Makefile
@@ -1,18 +1,22 @@
-LOADER_TMP= build/grub2.o build/loader.o build/os.iso build/iso/boot/loader.elf
+CFLAGS = -Wall -fpic -ffreestanding -fno-stack-protector -nostdinc -nostdlib -Ibootboot/dist/
+LDFLAGS =  -nostdlib -n -T link.ld
+STRIPFLAGS =  -s -K mmio -K fb -K bootboot -K environment -K initstack
 
-loader:
-	nasm -f elf32 src/grub2.s -o build/grub2.o
-	gcc -std=c99 -m32 -ffreestanding -fno-stack-protector -mno-red-zone -c src/loader.c -o build/loader.o
-	ld -T src/loader.ld -melf_i386 build/grub2.o build/loader.o -o build/iso/boot/loader.elf
+all: kernel.elf
 
-iso: loader
-	grub-mkrescue -o build/os.iso build/iso/
+kernel.elf:
+	mkdir -p boot
+	x86_64-elf-gcc $(CFLAGS) -mno-red-zone -c src/kernel.c -o kernel.o
+	x86_64-elf-ld -r -b binary -o font.o font.psf
+	x86_64-elf-ld $(LDFLAGS) kernel.o font.o -o boot/kernel.elf
+	x86_64-elf-strip $(STRIPFLAGS) boot/kernel.elf
+	x86_64-elf-readelf -hls boot/kernel.elf > kernel.txt
 
-run: iso
-	qemu-system-x86_64 -cdrom build/os.iso
+image: kernel.elf
+	bootboot/mkbootimg/mkbootimg mkbootimg.json cheetah.img
 
-debug: iso
-	qemu-system-x86_64 -cdrom build/os.iso -monitor stdio -s -S
+run: image
+	qemu-system-x86_64 -hda cheetah.img
 
 clean:
-	rm -rf $(LOADER_TMP)
+	rm -rf *.o *.txt boot cheetah.img || true
--- src/kernel.c
+++ src/kernel.c
@@ -1,8 +1,56 @@
+void puts(char *s);
 
-// 64-bit
+typedef short int int16_t;
+typedef unsigned char uint8_t;
+typedef unsigned short int uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long int uint64_t;
 
-// TODO
-//  IDT, CONTEXT SWITCH
-void main(void) {
+#include <bootboot.h>
 
+extern BOOTBOOT bootboot;               // see bootboot.h
+extern unsigned char environment[4096]; // configuration, UTF-8 text key=value pairs
+extern uint8_t fb;                      // linear framebuffer mapped
+
+void _start() {
+    puts("Cheetah Kernel");
+    while (1);
+}
+
+typedef struct {
+    uint32_t magic;
+    uint32_t version;
+    uint32_t headersize;
+    uint32_t flags;
+    uint32_t numglyph;
+    uint32_t bytesperglyph;
+    uint32_t height;
+    uint32_t width;
+    uint8_t glyphs;
+} __attribute__((packed)) psf2_t;
+extern volatile unsigned char _binary_font_psf_start;
+
+void puts(char *s) {
+    psf2_t *font = (psf2_t *) &_binary_font_psf_start;
+    int x, y, kx = 0, line, mask, offs;
+    int bpl = (font->width + 7) / 8;
+    while (*s) {
+        unsigned char *glyph = (unsigned char *) &_binary_font_psf_start + font->headersize +
+                               (*s > 0 && *s < font->numglyph ? *s : 0) * font->bytesperglyph;
+        offs = (kx * (font->width + 1) * 4);
+        for (y = 0; y < font->height; y++) {
+            line = offs;
+            mask = 1 << (font->width - 1);
+            for (x = 0; x < font->width; x++) {
+                *((uint32_t *) ((uint64_t) &fb + line)) = ((int) *glyph) & (mask) ? 0xFFFFFF : 0;
+                mask >>= 1;
+                line += 4;
+            }
+            *((uint32_t *) ((uint64_t) &fb + line)) = 0;
+            glyph += bpl;
+            offs += bootboot.fb_scanline;
+        }
+        s++;
+        kx++;
+    }
 }
