61c3f0e8327cc54f0c926a3e1c80ed0ec3ed51a3
Mohammad Amin Ahmadi <0x9n0p@gmail.com>
2023-10-25 21:57:52 +0330

    Load loader into 0x7e00

--- Makefile
+++ Makefile
@@ -1,12 +1,13 @@
 bootloader:
 	nasm -f bin -o build/bootloader.bin bootloader/bootloader.s
+	#nasm -f bin -o build/loader.bin bootloader/loader.s
 
 debug: bootloader
 	qemu-system-x86_64 -m 1024 build/bootloader.bin -S -s &
 	gdb build/bootloader.elf -ex 'source .breakpoints' -ex 'target remote localhost:1234'
 
 run: bootloader
-	qemu-system-i386 -m 1024 build/bootloader.bin
+	qemu-system-x86_64 -m 1024 build/bootloader.bin
 
 debug-rm: bootloader
 	qemu-system-i386  build/bootloader.bin -s -S &
--- bootloader/bootloader.s
+++ bootloader/bootloader.s
@@ -1,9 +1,7 @@
 [BITS 16]
 [ORG 0x7c00]
 
-jmp init
-
-init:
+boot_sector:
     xor ax, ax
     mov ds, ax
     mov es, ax
@@ -12,9 +10,56 @@ init:
     mov ss, ax
     mov sp, 0x7c00
 
-end:
+    call clear_screen
+
+    mov ah, 2 ; Read sector command
+    mov al, 2 ; n sector to read
+    mov ch, 0 ; Cylinder low eight bits
+    mov cl, 2 ; Read sector n
+    mov dh, 0 ; Head number
+    mov bx, 0x7e00
+    int 0x13
+    jc err_load_loader
+
+    jmp 0x7e00
+
+err_load_loader:
+    mov si, MSG_FAILED_LOAD_LOADER
+    call print
+    cli
+    hlt
+
+MSG_FAILED_LOAD_LOADER: db "* Failed to load loader", 0ah, 0dh, 0
+
+print:
+      pusha
+      mov bx, 0
+.loop:
+      lodsb
+      cmp al, 0
+      je .done
+      call print_char
+      jmp .loop
+.done:
+      popa
+      ret
+
+print_char:
+      mov ah, 0eh
+      int 0x10
+      ret
+
+clear_screen:
+      mov al, 02h
+      mov ah, 00h
+      int 10h
+      ret
+
+times 510-($-$$) db 0
+dw 0xAA55
+
+loader:
     hlt
     jmp $
 
-times 510-($-$$) db 0
-dw 0xaa55
+times 2048 db 0
